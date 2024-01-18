24de89f0786894880bed7911672cafbaaea41530
n-hadi <77697181+n-hadi@users.noreply.github.com>
2024-01-18 21:51:22 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 def distance2penalty(dist):
-    #-e^(x-2.4)+1.091
-    #result = -np.exp(dist - 2.4) + 1.091
     result = (np.exp(dist/1000) - 1) / (1 - np.exp(2.5)) + 1
     return result if result > 0 else 0
 
